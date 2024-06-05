8cfc965a66b0e1719b4fdae91d5ea554d2f92c90
Ana <ana.m.fucek@gmail.com>
2024-06-05 20:45:33 +0300

    dillllll

--- script.js
+++ script.js
@@ -17,32 +17,54 @@ function getHumanChoice () {
     let something = prompt('rock, paper, scissors?');
     return something.toLowerCase()
 }   
-
 console.log(getHumanChoice);
 
 let humanScore = 0
  let computerScore = 0
+ const disp = document.querySelector('.scoredisplay');
+ 
+
+
+
+const res = document.querySelector(".resultdisplay");
+    
 
  function playRound (humanChoice, computerChoice){
+
+    if (humanScore >= 3){
+        disp.textContent = "you won the game"
+            
+         } else if (computerScore >= 3) {
+            disp.textContent = "computer won the game"
+           
+         }
+
     if (humanChoice === 'rock' && computerChoice ==='paper'){
         computerScore ++
+        res.textContent = "You Lose! Paper beats Rock"
          return 'You Lose! Paper beats Rock'
     } else if (humanChoice === 'paper' && computerChoice === 'rock'){
         humanScore ++
+        res.textContent = "You win!! Paper beats Rock"
         return 'You win!! Paper beats Rock'
     } else if (humanChoice === computerChoice){
+        res.textContent = "Draw! You both have the same value!"
         return  'Draw! You both have the same value!'
     } else if (humanChoice === 'rock' && computerChoice ==='scissors') {
         humanScore ++
+        res.textContent = "You Win! Rock beats Scissors"
         return 'You Win! Rock beats Scissors'
     } else if (humanChoice === 'scissors' && computerChoice ==='rock') {
         computerScore ++
+        res.textContent = "you Lose!!! Rock beats Scissors"
         return 'you Lose!!! Rock beats Scissors'
     } else if ( humanChoice === 'scissors' && computerChoice === 'paper'){
         humanChoice ++
+        res.textContent = "You Win! Scissors beat Paper"
         return 'You Win! Scissors beat Paper'
     } else if (humanChoice === 'paper' && computerChoice === 'scissors'){
         computerChoice ++
+        res.textContent = "You Lose!!! Scissors beat Papaer"
         return 'You Lose!!! Scissors beat Papaer'
     }
  }
@@ -50,34 +72,22 @@ let humanScore = 0
 
 
 function playGame (){
-
-    let humanChoice = getHumanChoice()
-    let computerChoice = getComputerChoice()
-    playRound(humanChoice, computerChoice)
-
-
-    humanChoice = getHumanChoice()
-    computerChoice = getComputerChoice()
-
-    playRound(humanChoice, computerChoice)
-    humanChoice = getHumanChoice()
-    computerChoice = getComputerChoice()
     
-    playRound(humanChoice, computerChoice)
-    humanChoice = getHumanChoice()
-    computerChoice = getComputerChoice()
-    
-    playRound(humanChoice, computerChoice)
-    humanChoice = getHumanChoice()
-    computerChoice = getComputerChoice()
-    playRound(humanChoice, computerChoice)
 
-   if(humanScore > computerScore){
-    console.log('i')
-   } else {
-    console.log('f')
-   }
+     
      
 }
 console.log(playGame());
+
+const mkm = document.body.querySelector(".oneb");
+    mkm.addEventListener ("click", function() {playRound('rock', getComputerChoice())})
+
+
+const two = document.body.querySelector(".twob")
+    two.addEventListener ("click", function() {playRound('paper', getComputerChoice()) });
+
+
+ const tr = document.body.querySelector(".bb")
+    tr.addEventListener ("click", function() {playRound ('scissors', getComputerChoice())});
+
 
